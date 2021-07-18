9b0cf59ba6a5b1c86eaaf70c475dd3f5a3d561a7
ZippyMagician <zippymagician1@gmail.com>
2021-07-17 22:46:35 -0400

    Make binary usable

--- pith-bin/src/main.rs
+++ pith-bin/src/main.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    pith_lib::parse(":0*:0,2\n*:0,3\n=:1+*\n=&2%=+,1:1-*:0,4\n=&2%=+,1:1-*:0,5", &[]);
+    let matches = clap_app!(pith =>
+        (version: "1.0.0")
+        (about: "The interpreter for the pith esolang")
+        (@arg INPUT: +required "The input file to be run")
+        (@arg STDIN: "STDIN for the program")
+        (@arg numbers: -n "STDIN is a sequence of space-seperated numbers, instead of a sequence of characters")
+    )
+    .get_matches();
+
+    let file = matches.value_of("INPUT").unwrap();
+    let stdin = matches.value_of("STDIN").unwrap_or_default();
+    let stdin = if matches.is_present("numbers") {
+        stdin
+            .split(' ')
+            .map(|n| n.parse().expect("Invalid number(s) passed to STDIN"))
+            .collect()
+    } else {
+        stdin.as_bytes().to_owned()
+    };
+    if let Ok(program) = std::fs::read_to_string(file) {
+        pith_lib::parse(&program, &stdin);
+    } else {
+        panic!("File {} not found", file);
+    }
 }
